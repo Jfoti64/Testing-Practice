6ffe80dd8af0c3dcd3a60a1e2d3ab1d4b45a1ac4
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-20 04:39:43 -0500

    Add caesarCipher to index

--- src/index.js
+++ src/index.js
@@ -20,9 +20,35 @@ const calculate = {
     }
     return a / b;
   },
-  multiply: function(a, b) {
+  multiply: function (a, b) {
     return a * b;
-  }
+  },
 };
 
-export { capitalize, reverse, calculate };
+function caesarCipher(string, shiftFactor) {
+  // Adjust the shift factor to prevent irrelevant full cycles (e.g., shifting by 26 is the same as shifting by 0)
+  shiftFactor = shiftFactor % 26;
+
+  let modifiedString = string
+    .split('')
+    .map((char) => {
+      let code = char.charCodeAt(0);
+
+      // Shift lowercase letters
+      if (code >= 97 && code <= 122) {
+        code = ((code - 97 + shiftFactor) % 26) + 97;
+      }
+      // Shift uppercase letters
+      else if (code >= 65 && code <= 90) {
+        code = ((code - 65 + shiftFactor) % 26) + 65;
+      }
+      // Convert the modified char codes back to characters
+      return String.fromCharCode(code);
+    })
+    .join('');
+
+  return modifiedString;
+}
+
+caesarCipher('hello', 2);
+export { capitalize, reverse, calculate, caesarCipher };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverse, calculate } from './index';
+import { capitalize, reverse, calculate, caesarCipher } from './index';
 
 test('capitalize first letter of string', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -15,9 +15,18 @@ test('calculate the correct output', () => {
   expect(calculate.add(2, 5)).toBe(7);
   expect(calculate.add(-2, 12)).toBe(10);
   expect(calculate.subtract(10, 5)).toBe(5);
-  expect(calculate.subtract(20, .5)).toBe(19.5);
+  expect(calculate.subtract(20, 0.5)).toBe(19.5);
   expect(calculate.divide(10, 5)).toBe(2);
   expect(() => calculate.divide(5, 0)).toThrow(Error);
   expect(calculate.multiply(5, 5)).toBe(25);
   expect(calculate.multiply(-5, 2)).toBe(-10);
+});
+
+test('shift string char by shift factor', () => {
+  expect(caesarCipher('hello', 2)).toBe('jgnnq');
+  expect(caesarCipher('hi', 2)).toBe('jk');
+  expect(caesarCipher('hello', 4)).toBe('lipps');
+  expect(caesarCipher('HeLlO', 4)).toBe('LiPpS');
+  expect(caesarCipher('zebra', 5)).toBe('ejgwf');
+  expect(caesarCipher('Zebra', 5)).toBe('Ejgwf');
 });
